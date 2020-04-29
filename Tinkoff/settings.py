d7f12d273007d6244b109f84f11ad33b6224ef68
Zurshak <64207630+Zurshak@users.noreply.github.com>
2020-04-29 21:10:53 +0300

    v 1.0 complete

--- Tinkoff/settings.py
+++ Tinkoff/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sb*7d&%!n!nd-ahl^i6&t8uj0(flvc5-nb5-dux7vtyc^yd0l@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.138.85','84.201.138.85:8000','84.201.138.85:80']
+ALLOWED_HOSTS = ['84.201.138.85','84.201.138.85:8000','84.201.138.85:80','127.0.0.1:8000','127.0.0.1']
 
 
 # Application definition
@@ -119,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = 'home/zurshak/Invest/static'
 MEDIA_ROOT = 'home/zurshak/Invest/media'
